a0102ca337af6f86bd6c6b283425c17b62056e4f
evertonfrey <everton.frey.2016@gmail.com>
2020-08-01 18:16:22 -0300

    Continuação secção ofertas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import Header from './components/Header';
 import Carrossel from './components/Carrossel'
 import './styles.css';
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import Ofertas from './components/Ofertas';
 
 
--- src/components/Ofertas/index.js
+++ src/components/Ofertas/index.js
@@ -1,34 +1,80 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+// import Badge from "@material-ui/core/Badge";
 
-import './styles.css'; // src/components/Ofertas/styles.css
+import Grid from "@material-ui/core/Grid";
 
-import selo1 from '../../assets/img/Icone-oferta-segunda.png';
-import selo2 from '../../assets/img/Icone-verde.png';
-import selo3 from '../../assets/img/Icone-azul.png';
-import selo4 from '../../assets/img/icone-sextou.png';
+import "./styles.css"; // src/components/Ofertas/styles.css
+
+import selo1 from "../../assets/img/Icone-oferta-segunda.png";
+import selo2 from "../../assets/img/Icone-verde.png";
+import selo3 from "../../assets/img/Icone-azul.png";
+import selo4 from "../../assets/img/icone-sextou.png";
+
+let ListaOfertas = [
+  {
+    descricao: "",
+    validade: "",
+    path: "",
+    imagem: "",
+  },
+];
+
+ListaOfertas.push({
+  descricao: "Segunda e terça",
+  imagem: "Icone-oferta-segunda.png",
+  path: "../../assets/img/Icone-oferta-segunda.png",
+});
+
+console.log(ListaOfertas[1].path);
 
 const Ofertas = () => (
-    <div id="menu">
-        <ul>
-            <li>
-                <img src={selo1}/>
-                <p>Segunda e Terça</p>
-            </li>
-            <li>
-                <img src={selo2}/>
-                <p>Quarta e Quinta</p>
-            </li>
-            <li>
-                <img src={selo3}/>
-                <p>Sexta e Sábado</p>
-            </li>
-            <li>
-                <img src={selo4}/>
-                <p>Sextou do Real</p>
-            </li>
-             
-        </ul>
+  <div className="ofertas">
+    <Grid container spacing={12}>
+      <Grid item xs={12} sm={2}>
+        <img className="p-col" src={selo1} alt="Segunda e Terça" />
+        <p>20/07 a 21/07</p>
+      </Grid>
+      <Grid item xs={12} sm={2}>
+        <img className="p-col" src={selo2} alt="Quarta e Quinta" />
+        <p>20/07 a 21/07</p>
+      </Grid>
+      <Grid item xs={12} sm={2}>
+        <img className="p-col" src={selo3} alt="Sexta e Sábado" />
+        <p>20/07 a 21/07</p>
+      </Grid>
+      <Grid item xs={12} sm={2}>
+        <img className="p-col" src={selo4} alt="Sextou Real" />
+        <p>20/07 a 21/07</p>
+      </Grid>
+
+      {/* <Grid item xs={6} sm={3}>
+        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+      </Grid>
+      <Grid item xs={6} sm={3}>
+        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+      </Grid>
+      <Grid item xs={6} sm={3}>
+        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+      </Grid>
+      <Grid item xs={6} sm={3}>
+        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+      </Grid> */}
+    </Grid>
+
+    {/* <div>
+      <img className="p-col" src={selo1} alt="Segunda e Terça" />
     </div>
+    <div>
+      <img className="p-col" src={selo2} alt="Quarta e Quinta" />
+    </div>
+    <div>
+      <img className="p-col" src={selo3} alt="Sexta e Sábado" />
+    </div>
+    <div>
+      <img className="p-col" src={selo4} alt="Sextou Real" />
+    </div> */}
+  </div>
 );
 
 export default Ofertas;
