c6ccf4889a0a2e6a840520690027019057f70d73
everton.frey <everton.frey.2016@gmail.com>
2020-08-04 23:55:15 -0300

    continuação

--- src/components/Ofertas/index.js
+++ src/components/Ofertas/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import {Img} from 'react-image';
 // import Badge from "@material-ui/core/Badge";
 
 import Grid from "@material-ui/core/Grid";
 
 import "./styles.css"; // src/components/Ofertas/styles.css
+
+
 
 import selo1 from "../../assets/img/Icone-oferta-segunda.png";
 import selo2 from "../../assets/img/Icone-verde.png";
@@ -27,12 +30,19 @@ ListaOfertas.push({
 });
 
 console.log(ListaOfertas[1].path);
+//  const pathimg = "../../assets/img/Icone-oferta-segunda.png"
+//  const imagem = require(pathimg);
+
+// const imagem = 'http://localhost:3000/src/assets/img/Icone-oferta-segunda.png'
+
 
 const Ofertas = () => (
   <div className="ofertas">
     <Grid container spacing={12}>
       <Grid item xs={12} sm={2}>
         <img className="p-col" src={selo1} alt="Segunda e Terça" />
+        {/* <Img src={require(`${pathimg}`)}></Img> */}
+        {/* <img className="p-col" src={imagem} alt="Segunda e Terça" /> */}
         <p>20/07 a 21/07</p>
       </Grid>
       <Grid item xs={12} sm={2}>
